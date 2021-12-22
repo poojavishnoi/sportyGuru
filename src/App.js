314a9ebb4dc19d9684ba09aa4793fcd44770f5d6
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2021-12-22 19:59:41 +0530

    Internship Assignment from Sporty Guru

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+
+  const url = 'http://universities.hipolabs.com/search?country=India'
+  const [details, setDetails] = useState([])
+
+  useEffect(() => {
+    fetch(url)
+      .then(resp => {
+        return resp.json()
+      })
+      .then(data => {
+        let obj = [];
+        for(let i=0; i < data.length; i++){
+          obj.push({'name':data[i].name, 'link':data[i].web_pages[0]})
+          
+        }
+        setDetails(obj)
+      }
+    )
+    },[]
+  )
+
+  console.log(details)
+
+  return (
+    <div className="App">
+      <h1 className='header'>Universities in India</h1>
+      <div className='university_link'>
+
+      {
+        details.map(
+          detail => {
+            return (
+              <div>
+                <li className='list'>
+                <a href={detail.link} target={"_blank"} rel={'noopener noreferrer'}>{detail.name}</a>
+                </li>
+              </div>
+            )
+          
+        }
+        )
+      }
+      </div>
+    </div>
+  );
+}
+
+export default App;
